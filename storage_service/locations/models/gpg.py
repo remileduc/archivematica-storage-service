c2a67273441baa93219fd65b1ef46f33c3909534
Joel Dunham <jrwdunham@gmail.com>
2017-02-08 12:00:54 -0800

    AIP encryption WIP: pointer file mods

--- storage_service/locations/models/gpg.py
+++ storage_service/locations/models/gpg.py
@@ -2,18 +2,24 @@ from __future__ import absolute_import
 # stdlib, alphabetical
 import datetime
 import logging
+from lxml import etree
+from lxml.builder import ElementMaker
 import os
+from uuid import uuid4
 
 # Core Django, alphabetical
 from django.db import models
+from django.utils import timezone
 
 # Third party dependencies, alphabetical
 import gnupg
 
 # This project, alphabetical
+from common import utils
 
 # This module, alphabetical
 from .location import Location
+from .package import Package
 
 
 LOGGER = logging.getLogger(__name__)
@@ -26,58 +32,215 @@ GPG_KEY_REAL_NAME = 'Archivematica Key'
 GPG_KEY_PASSPHRASE = ''
 
 
+class GPGException(Exception):
+    pass
+
+
 class GPG(models.Model):
-    """Space for storing things as files encrypted via GnuPG.
+    """Space for storing things as files encrypted via GnuPG."""
 
-    Testing TODOs. Places where ``move_to_storage_service`` is called:
-
-    - package.py::recover_aip
-      - locations/views.py::aip_recover_request
-    - package.py::store_aip
-    - package.py::backlog_transfer
-    - package.py::start_reingest DONE (works)
-    - package.py::finish_reingest (twice)
-    - package.py::fetch_local_path
-    - resources.py::post_detail
-
-    """
     space = models.OneToOneField('Space', to_field='uuid')
 
     class Meta:
         verbose_name = "GPG encryption on Local Filesystem"
         app_label = 'locations'
 
+    # Parsed pointer file
+    pointer_root = None
+
     ALLOWED_LOCATION_PURPOSE = [
-        Location.AIP_STORAGE,
-        #Location.DIP_STORAGE,
-        #Location.BACKLOG,
+        Location.AIP_STORAGE
     ]
 
-    def move_to_storage_service(self, src_path, dst_path, dest_space):
-        """Moves src_path to dest_space.staging_path/dst_path. Note: we
-        implicitly assume that the encrypted file in this space has the '.gpg'
-        extension. After transport to the storage service, the decrypted file
-        will lack this extension.
+    def move_to_storage_service(self, src_path, dst_path, dst_space):
+        """Moves src_path to dst_path.
+        Note: we implicitly assume that the encrypted file in this space has
+        the '.gpg' extension. After transport to the storage service, the
+        decrypted file will lack this extension.
         """
-        src_path_encr = src_path + '.gpg'
-        dst_path_encr = dst_path + '.gpg'
         LOGGER.debug('GPG ``move_to_storage_service``')
         LOGGER.debug('GPG move_to_storage_service encrypted src_path: %s',
-                     src_path_encr)
+                     src_path)
         LOGGER.debug('GPG move_to_storage_service encrypted dst_path: %s',
-                     dst_path_encr)
-        self.space.create_local_directory(dst_path_encr)
-        self.space.move_rsync(src_path_encr, dst_path_encr)
-        self._gpg_decrypt(dst_path)
+                     dst_path)
+
+        #src_path_encr = src_path + '.gpg'
+        #dst_path_encr = dst_path + '.gpg'
+
+        self.space.create_local_directory(dst_path)
+        self.space.move_rsync(src_path, dst_path)
+        #decr_path = self._gpg_decrypt(dst_path)
+
 
     def move_from_storage_service(self, src_path, dst_path, package=None):
         """Moves self.staging_path/src_path to dst_path. """
         LOGGER.debug('in move_from_storage_service of GPG')
         LOGGER.debug('GPG move_from, src_path: %s', src_path)
         LOGGER.debug('GPG move_from, dst_path: %s', dst_path)
+        if not package:
+            raise GPGException('GPG spaces can only contain packages')
         self.space.create_local_directory(dst_path)
         self.space.move_rsync(src_path, dst_path, try_mv_local=True)
-        self._gpg_encrypt(dst_path)
+        try:
+            encr_path = self._gpg_encrypt(dst_path)
+        except GPGException:
+            # If we fail to encrypt, we move it back. Is this what should be done?
+            self.space.move_rsync(dst_path, src_path, try_mv_local=True)
+            raise
+        self._update_package(package, encr_path)
+
+    def _update_package(self, package, encr_path):
+        """
+        - update the package with the new current_path
+        - update the pointer file ...
+        TODO/QUESTION: should this be done here (in the SS) or in a new
+        micro-service chain link, e.g., one that occurs after "Store the AIP"?
+        """
+
+        # If we update the Package's current_path to the encrypted path here
+        # and save it in the db, then package.py::Package.store_aip will update
+        # mets:file/mets:FLocat[@xlink:href] appropriately.
+        package.current_path = encr_path
+        package.save()
+        # Update pointer file to contain a record of the encryption.
+        # TODO/QUESTION: Allow for AICs too?
+        if (    package.pointer_file_path and
+                package.package_type in (Package.AIP,)):
+            pointer_absolute_path = package.full_pointer_file_path
+            root = etree.parse(pointer_absolute_path)
+            metsBNS = "{" + utils.NSMAP['mets'] + "}"
+            # Add a new <mets:transformFile> under the <mets:file> for the AIP,
+            # one which indicates that a decryption transform is needed.
+            file_el = root.find('.//mets:file', namespaces=utils.NSMAP)
+            if package.uuid in file_el.get('ID', ''):
+                # TODO/QUESTION: for compression with 7z using bzip2, the
+                # algorithm is "bzip2". Should the algorithm here be "gpg" or
+                # "AES256"?
+                algorithm = 'gpg'
+                # TODO/QUESTION: add a TRANSFORMKEY attr with the id of the GPG
+                # private key needed to decrypt this AIP? From METS docs: "A
+                # key to be used with the transform algorithm for accessing the
+                # file's contents."
+                etree.SubElement(file_el, metsBNS + "transformFile",
+                    TRANSFORMORDER='1',
+                    TRANSFORMTYPE='decryption',
+                    TRANSFORMALGORITHM=algorithm)
+                # Decompression <transformFile> must have its TRANSFORMORDER
+                # attr changed to '2', because decryption is a precondition to
+                # decompression.
+                decompr_transform_el = file_el.find(
+                    'mets:transformFile[@TRANSFORMTYPE="decompression"]',
+                    namespaces=utils.NSMAP)
+                if decompr_transform_el is not None:
+                    decompr_transform_el.set('TRANSFORMORDER', '2')
+
+            # Add a <PREMIS:EVENT> for the encryption event
+            # TODO/QUESTION: the pipeline is usually responsible for creating
+            # these things in the pointer file. The createPointerFile.py client
+            # script, in particular, creates these digiprovMD elements based on
+            # events and agents in the pipeline's database. In this case, we are
+            # encrypting in the storage service and creating PREMIS events in the
+            # pointer file that are *not* also recorded in the database (SS's or
+            # AM's). Seems like maybe encryption should occur as a micro-service
+            # in the pipeline.
+            amdsec = root.find('.//mets:amdSec', namespaces=utils.NSMAP)
+            # E = ElementMaker(namespace=utils.NSMAP['mets'], nsmap=utils.NSMAP)
+            # etree.SubElement(amdsec
+            next_digiprov_md_id = self.get_next_digiprov_md_id(root)
+            print('next digiprovMD ID: {}'.format(next_digiprov_md_id))
+            digiprovMD = etree.Element(
+                metsBNS + 'digiprovMD',
+                ID=next_digiprov_md_id)
+            mdWrap = etree.SubElement(
+                digiprovMD,
+                metsBNS + 'mdWrap',
+                MDTYPE='PREMIS:EVENT')
+            xmlData = etree.SubElement(mdWrap, metsBNS + 'xmlData')
+            xmlData.append(self.create_encr_event(root))
+            amdsec.append(digiprovMD)
+
+            with open(pointer_absolute_path, 'w') as fileo:
+                fileo.write(etree.tostring(root, pretty_print=True))
+
+    def create_encr_event(self, root):
+        """Returns a PREMIS Event for the encryption."""
+        # The following vars would typically come from an AM Events model.
+        encr_event_type = 'encryption'
+        encr_event_uuid = str(uuid4())
+        encr_event_datetime = timezone.now().isoformat()
+        encr_event_detail = escape(
+            'program=python-gnupg; version={}'.format(gnupg.__version__))
+        premisNS = utils.NSMAP['premis']
+        premisBNS = '{' + premisNS + '}'
+        xsiNS = utils.NSMAP['xsi']
+        xsiBNS = '{' + xsiNS + '}'
+        event = etree.Element(
+            premisBNS + 'event', nsmap={'premis': premisNS})
+        event.set(xsiBNS + 'schemaLocation',
+                    premisNS + ' http://www.loc.gov/standards/premis/'
+                                'v2/premis-v2-2.xsd')
+        event.set('version', '2.2')
+        eventIdentifier = etree.SubElement(
+            event,
+            premisBNS + 'eventIdentifier')
+        etree.SubElement(
+            eventIdentifier,
+            premisBNS + 'eventIdentifierType').text = 'UUID'
+        etree.SubElement(
+            eventIdentifier,
+            premisBNS + 'eventIdentifierValue').text = encr_event_uuid
+        etree.SubElement(
+            event,
+            premisBNS + 'eventType').text = encr_event_type
+        etree.SubElement(
+            event,
+            premisBNS + 'eventDateTime').text = encr_event_datetime
+        etree.SubElement(
+            event,
+            premisBNS + 'eventDetail').text = encr_event_detail
+        eventOutcomeInformation = etree.SubElement(
+            event,
+            premisBNS + 'eventOutcomeInformation')
+        etree.SubElement(
+            eventOutcomeInformation,
+            premisBNS + 'eventOutcome').text = '' # No eventOutcome text at present ...
+        eventOutcomeDetail = etree.SubElement(
+            eventOutcomeInformation,
+            premisBNS + 'eventOutcomeDetail')
+        etree.SubElement(
+            eventOutcomeDetail,
+            premisBNS + 'eventOutcomeDetailNote').text = escape(
+                'Yay, GnuPG encryption worked!')
+            # Copy the existing <premis:agentIdentifier> data to
+            # <premis:linkingAgentIdentifier> elements in our encryption
+            # <premis:event>
+        for agent_id_el in root.findall(
+                './/premis:agentIdentifier', namespaces=utils.NSMAP):
+            agent_id_type = agent_id_el.find('premis:agentIdentifierType',
+                                                namespaces=utils.NSMAP).text
+            agent_id_value = agent_id_el.find('premis:agentIdentifierValue',
+                                                namespaces=utils.NSMAP).text
+            linkingAgentIdentifier = etree.SubElement(
+                event,
+                premisBNS + 'linkingAgentIdentifier')
+            etree.SubElement(
+                linkingAgentIdentifier,
+                premisBNS + 'linkingAgentIdentifierType').text = agent_id_type
+            etree.SubElement(
+                linkingAgentIdentifier,
+                premisBNS + 'linkingAgentIdentifierValue').text = agent_id_value
+        return event
+
+    def get_next_digiprov_md_id(self, root):
+        ids = []
+        for digiprov_md_el in root.findall(
+                './/mets:digiprovMD', namespaces=utils.NSMAP):
+            digiprov_md_id = int(digiprov_md_el.get('ID').replace(
+                'digiprovMD_', ''))
+            ids.append(digiprov_md_id)
+        if ids:
+            return 'digiprovMD_{}'.format(max(ids) + 1)
+        return 'digiprovMD_1'
 
     def _browse(self, path):
         """At present we are not implementing a ``browse`` method for ``GPG``.
@@ -95,17 +258,15 @@ class GPG(models.Model):
 
     def _gpg_encrypt(self, path):
         """Use GnuPG to encrypt the file at ``path`` in situ. Note: we
-        implicitly suffix the '.gpg' extension to the encrypted file at
-        ``path``.
+        add a '.gpg' extension to the encrypted file at ``path``.
         Questions:
         - Should the unencrypted file at ``path`` be destroyed
           post-encryption (as is currently done)?
-        - How to handle ``path`` as directory? Right now raising a general
-          Exception
+        - How to handle ``path`` as directory? Currently raising an exception
         """
         LOGGER.debug('Encrypting %s.', path)
         if os.path.isdir(path):
-            raise Exception(
+            raise GPGException(
                 'GPG cannot encrypt a directory. Archive {} first!'.format(
                     path))
         key = self._get_key()
@@ -120,8 +281,12 @@ class GPG(models.Model):
         if os.path.isfile(encr_path):
             LOGGER.debug('Successfully encrypted %s', path)
             os.remove(path)
+            return encr_path
         else:
             LOGGER.debug('Failed to encrypt %s; storing it unencrypted.', path)
+            raise GPGException(
+                'Something went wrong when attempting to encrypt'
+                ' {}'.format(path))
 
     def _gpg_decrypt(self, path):
         """Use GnuPG to decrypt the file at path in situ.
@@ -129,21 +294,22 @@ class GPG(models.Model):
         an encrypted AIP. However, it appears that doing so does not trigger
         the calling of ``move_to_storage_service``...
         """
-        encr_path = path + '.gpg'
-        LOGGER.debug('Decrypting %s.', encr_path)
-        if not os.path.isfile(encr_path):
-            LOGGER.error('There is no path at %s (%s) to decrypt.', path,
-                         encr_path)
-            raise Exception('Cannot decrypt file at {} ({}); no such'
-                            ' file.'.format(path, encr_path))
-        decr_path = path
-        with open(encr_path, 'rb') as stream:
+        #encr_path = path + '.gpg'
+        LOGGER.debug('Decrypting %s.', path)
+        if not os.path.isfile(path):
+            LOGGER.error('There is no path at %s to decrypt.', path)
+            raise GPGException('Cannot decrypt file at {}; no such'
+                            ' file.'.format(path))
+        decr_path, _ = os.path.splitext(path)
+        with open(path, 'rb') as stream:
             gpg.decrypt_file(stream, output=decr_path)
         if os.path.isfile(decr_path):
-            LOGGER.debug('Successfully decrypted %s (%s).', path, encr_path)
-            os.remove(encr_path)
+            LOGGER.debug('Successfully decrypted %s.', path)
+            os.remove(path)
+            return decr_path
         else:
-            LOGGER.debug('Failed to decrypt %s.', encr_path)
+            LOGGER.debug('Failed to decrypt %s.', path)
+            raise GPGException('Failed to decrypt file at {}'.format(path))
 
     def _get_key(self):
         """Check if our example key already exists. If it does, return it.
@@ -184,3 +350,13 @@ class GPG(models.Model):
         verified = os.path.isdir(self.space.path)
         self.space.verified = verified
         self.space.last_verified = datetime.datetime.now()
+
+
+# This replaces non-unicode characters with a replacement character,
+# and is primarily used for arbitrary strings (e.g. filenames, paths)
+# that might not be valid unicode to begin with.
+# NOTE: non-DRY from archivematicaCommon/archivematicaFunctions.py
+def escape(string):
+    if isinstance(string, str):
+        string = string.decode('utf-8', errors='replace')
+    return string
